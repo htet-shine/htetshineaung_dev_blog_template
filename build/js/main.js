4d90202780df0c5b8e9f42cbd80c9ce1ca78607a
Htet Shine Aung <htetshineaung@gmail.com>
2021-07-04 16:45:44 +0630

    css and js update

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,45 @@
 
+const emptyLinkAlert = () => {
+
+	const emptyDemoLink = () => {
+		const demoLink = document.querySelector('body .demolink');
+
+		if (demoLink != null) {
+			// get last string after '/'
+			const demoLastString = demoLink.href.substr(demoLink.href.lastIndexOf('/') + 1);	
+			demoLink.addEventListener('click', (e) => {
+
+				if (demoLastString == '#') {
+					e.preventDefault();
+					demoLink.innerHTML = "No Demo";
+					demoLink.classList.add('empty');
+				}
+
+			});
+		}	
+	}
+	emptyDemoLink();
+
+	const emptySourceLink = () => {
+		const sourceLink = document.querySelector('body .sourcelink');
+		
+		if (sourceLink != null) {
+			// get last string after '/'
+			const sourceLastString = sourceLink.href.substr(sourceLink.href.lastIndexOf('/') + 1);
+			sourceLink.addEventListener('click', (e) => {
+				if (sourceLastString == '#') {
+					e.preventDefault();
+					sourceLink.innerHTML = "No Source";
+					sourceLink.classList.add('empty');
+				}
+			});
+		}
+	}
+	emptySourceLink();
+
+}
+emptyLinkAlert();
+
 const navigation = () => {
 	
 	// Navbar Slide for Mobile
